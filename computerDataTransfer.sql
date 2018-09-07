6363dd74860ab2d29c105ae42abc146eec3eadc0
Kwanchai Eurviriyanukul <kwanchai@rmutl.ac.th>
2018-09-07 08:26:59 +0000

    add delete command to prevent duplicate insert to all tables

--- computerDataTransfer.sql
+++ computerDataTransfer.sql
@@ -2,9 +2,15 @@ use computer;
 delete from computer.Laptop;
 insert into computer.Laptop
  select model, speed, ram, hd, screen, price from computer1.Laptop;
+
+delete from computer.PC;
 insert into computer.PC
  select model, speed, ram, hd, price from computer1.PC;
+
+delete from computer.Printer;
 insert into computer.Printer
  select model, color, type, price from computer1.Printer;
+
+delete from computer.Product;
 insert into computer.Product
  select maker, model, type from computer1.Product;
