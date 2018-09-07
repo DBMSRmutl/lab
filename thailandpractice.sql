e82f649f21be64584ff2ae4ddf87f7e5d36633af
Kwanchai Eurviriyanukul <kwanchai@rmutl.ac.th>
2018-09-07 09:15:36 +0000

    add sql command for thailand questions.

---
+++ thailandpractice.sql
@@ -0,0 +1,15 @@
+-- list province name accending
+use thailand
+select province_name, province_ID
+from province
+order by convert (province_name using tis620);
+
+select village_name, subdistrict_name, district_name
+from villageThailand
+where subdistrict_name='ช้างเผือก' and province_name = 'เชียงใหม่';
+
+select village_name, subdistrict_name, district_name, province_name
+from villageThailand
+where subdistrict_name='อำเภอเมือง' and province_name = 'ลำปาง' 
+ or province_name = 'บึงกาฬ'
+order by population, subdistrict_name;
