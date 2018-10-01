5330aefba7a629c007795538ee1e7120b0eeed37
Kwanchai Eurviriyanukul <kwanchai@rmutl.ac.th>
2018-10-01 12:59:30 +0700

    add grant and drop user to sql command

--- usersAndPrivileges.sql
+++ usersAndPrivileges.sql
@@ -1,4 +1,12 @@
 use thailand;
 create user IF NOT EXISTS 'testUser'@'localhost' identified by '3333';
 
+create user if not exists myuser identified by '1234';
 
+grant all privileges on *.* to myuser with grant option;
+
+-- revoke all privileges on *.* from myuser;
+
+revoke all privileges, grant option from myuser;
+
+drop user if exists myuser;
